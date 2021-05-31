32f72aef6fa1ccc2e5e70d17dad97e87ea9d0225
jahid12ry <fhjahid720@gmail.com>
2021-05-31 11:51:04 +0600

    subtract function

---
+++ arti.py
@@ -0,0 +1,12 @@
+#Python program to subtract two numbers
+
+num1=56.8; #variable declaration and initialization
+num2=67.9;
+
+#Subtract two numbers
+sub=num2-num1;
+
+
+#display the result of substraction
+
+print("The subtract of {0} and {1} is {2}".format(num2,num1,sub) )
